bd733533d2276b18c4d2a00c90db4cb0a46dd2c5
Derian Molina Lopez <DerianMolina220@gmail.com>
2023-11-12 18:21:45 -0600

    arreglando el mapa

--- Pruebas/PruebasQuinceañeras/GrafoMapeado/Mapear.py
+++ Pruebas/PruebasQuinceañeras/GrafoMapeado/Mapear.py
@@ -8,26 +8,30 @@ class Mapeado:
         self.lineas = lineas
 
     def graficar_puntos(self):
-        # Cargar la imagen de fondo
-        background_img = mpimg.imread('IA/Pruebas/PruebasQuinceañeras/GrafoMapeado/Mapa.jpg')
+        try:
+            # Cargar la imagen de fondo
+            background_img = mpimg.imread('IA/IA/Pruebas/PruebasQuinceañeras/GrafoMapeado/Mapa.jpg')
 
-        # Crear el gráfico
-        plt.figure(figsize=(10, 8))
-        plt.imshow(background_img, extent=[-50, 50, -50, 50])
+            # Crear el gráfico
+            plt.figure(figsize=(10, 8))
+            plt.imshow(background_img, extent=[-50, 50, -50, 50])
 
-        for punto, coordenadas in self.puntos.items():
-            plt.plot(coordenadas[0], coordenadas[1], marker='o', markersize=8, color='blue')
-            plt.text(coordenadas[0], coordenadas[1], punto, fontsize=12, ha='right')
+            for punto, coordenadas in self.puntos.items():
+                plt.plot(coordenadas[0], coordenadas[1], marker='o', markersize=8, color='blue')
+                plt.text(coordenadas[0], coordenadas[1], punto, fontsize=12, ha='right')
 
-        # Graficar las líneas
-        self.graficar_lineas()
+            # Graficar las líneas
+            self.graficar_lineas()
 
-        plt.xlabel('Eje X')
-        plt.ylabel('Eje Y')
-        plt.title('Grafo')
-        plt.grid(False)
-        plt.axis('off')
-        plt.show()
+            plt.xlabel('Eje X')
+            plt.ylabel('Eje Y')
+            plt.title('Grafo')
+            plt.grid(False)
+            plt.axis('off')
+            plt.show()
+
+        except FileNotFoundError:
+            print("Error: No se pudo encontrar la imagen de fondo.")
 
     def graficar_lineas(self):
         for (ciudad1, ciudad2), propiedades in self.lineas.items():
@@ -35,7 +39,7 @@ class Mapeado:
             coords_ciudad2 = self.puntos[ciudad2]
             plt.plot([coords_ciudad1[0], coords_ciudad2[0]],
                      [coords_ciudad1[1], coords_ciudad2[1]],
-                     linestyle=propiedades['linestyle'],color='blue', linewidth=2)
+                     linestyle=propiedades['linestyle'], color='blue', linewidth=2)
 
 # Uso de la clase y graficación de puntos con imagen de fondo
 objeto = Mapeado()
